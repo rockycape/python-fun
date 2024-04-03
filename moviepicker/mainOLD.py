8d5e09552d16a8d62c0d0fb1b19a420760e723d5
rockycape <19771537+rockycape@users.noreply.github.com>
2024-04-03 13:33:56 +1100

    update

--- moviepicker/mainOLD.py
+++ moviepicker/mainOLD.py
@@ -3,11 +3,16 @@ import requests
 from bs4 import BeautifulSoup
 
 # crawl IMDB Top 250 and randomly select a movie
+
 URL = 'http://www.imdb.com/chart/top'
 
 def main():
     response = requests.get(URL)
+
     soup = BeautifulSoup(response.text, 'html.parser')
+    #soup = BeautifulSoup(response.text, 'lxml') # faster
+
+    # print(soup.prettify())
 
     movietags = soup.select('td.titleColumn')
     inner_movietags = soup.select('td.titleColumn a')
@@ -19,20 +24,21 @@ def main():
         return year
 
     years = [get_year(tag) for tag in movietags]
-    actors_list = [tag['title'] for tag in inner_movietags]  # access attribute 'title'
+    actors_list =[tag['title'] for tag in inner_movietags] # access attribute 'title'
     titles = [tag.text for tag in inner_movietags]
-    ratings = [float(tag['data-value']) for tag in ratingtags]  # access attribute 'data-value'
+    ratings = [float(tag['data-value']) for tag in ratingtags] # access attribute 'data-value'
 
     n_movies = len(titles)
 
-    while True:
+    while(True):
         idx = random.randrange(0, n_movies)
         
         print(f'{titles[idx]} {years[idx]}, Rating: {ratings[idx]:.1f}, Starring: {actors_list[idx]}')
 
-        user_input = input('Do you want another movie (y/[n])? ').strip().lower()  # convert input to lowercase and remove leading/trailing whitespace
-        if not user_input or user_input == 'n':
+        user_input = input('Do you want another movie (y/[n])? ')
+        if user_input != 'y':
             break
+    
 
 if __name__ == '__main__':
     main()
